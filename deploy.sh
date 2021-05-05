4f9fb0245768f23b532fbc546a329f8c82efaafc
Kristian Freeman <kristian@kristianfreeman.com>
2021-05-05 11:56:34 -0500

    Check env vars in deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,25 @@
 #!/bin/bash
 
+if [[ -z "$CF_ACCOUNT_ID" ]]; then
+  echo "Missing CF_ACCOUNT_ID" 1>&2
+  exit 1
+fi
+
+if [[ -z "$CF_API_KEY" ]]; then
+  echo "Missing CF_API_KEY" 1>&2
+  exit 1
+fi
+
+if [[ -z "$CF_EMAIL" ]]; then
+  echo "Missing CF_EMAIL" 1>&2
+  exit 1
+fi
+
+if [[ -z "$PROJECT_NAME" ]]; then
+  echo "Missing PROJECT_NAME" 1>&2
+  exit 1
+fi
+
 curl "https://api.cloudflare.com/client/v4/accounts/$CF_ACCOUNT_ID/pages/projects/$PROJECT_NAME/deployments" \
   -X POST \
   -s -o /dev/null \
